c7f33c0dcd029bf8212e1d26113561528f4b13ef
musicS2 <musics2@gmail.com>
2025-04-05 02:04:17 +0900

    공유하기 버튼 제작

---
+++ app/components/Button/UrlShareButton.tsx
@@ -0,0 +1,32 @@
+// import { ShareIcon } from '@heroicons/react/16/solid';
+import { ShareIcon } from '@heroicons/react/24/solid';
+import { usePathname } from 'next/navigation';
+
+function UrlShareButton() {
+  const pathname = usePathname();
+  console.log(pathname, 'path');
+  const link = `${pathname}`;
+
+  const shareData = {
+    // title: 'RIIZE STREAMING',
+    // text: 'RIIZE STREAMING',
+    url: link,
+  };
+
+  const handleUrlShare = async () => {
+    try {
+      await navigator.share(shareData);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <button className="btn-share" type="button" onClick={handleUrlShare}>
+      <i>
+        <ShareIcon />
+      </i>
+    </button>
+  );
+}
+export default UrlShareButton;
