9d92090195311c20173530c9bbea8e17410e117f
musicS2 <musics2@gmail.com>
2025-04-10 01:19:39 +0900

    가이드 / 차트 페이지 레이아웃 페이지 재생성

---
+++ app/(page)/guide/layout.tsx
@@ -0,0 +1,7 @@
+import { Container } from '@/components';
+
+function GuideLayout({ children }: { children: React.ReactNode }) {
+  return <Container>{children}</Container>;
+}
+
+export default GuideLayout;
