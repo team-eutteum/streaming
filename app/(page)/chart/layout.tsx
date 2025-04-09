0e3a1fdb217cc921d002432beb25905e7f00d80b
musicS2 <musics2@gmail.com>
2025-04-10 00:51:56 +0900

    가이드 / 차트 전체 목록 페이지 삭제

--- app/(page)/chart/layout.tsx
+++
@@ -1,7 +0,0 @@
-import { Container } from '@/components';
-
-function ChartLayout({ children }: { children: React.ReactNode }) {
-  return <Container>{children}</Container>;
-}
-
-export default ChartLayout;
--- app/(page)/guide/layout.tsx
+++
@@ -1,7 +0,0 @@
-import { Container } from '@/components';
-
-function GuideLayout({ children }: { children: React.ReactNode }) {
-  return <Container>{children}</Container>;
-}
-
-export default GuideLayout;
