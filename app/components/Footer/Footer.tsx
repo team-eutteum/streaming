2171c6f42fbafd2cc899958fad08180d01585cb6
chobee-joy <chobee0904@gmail.com>
2025-04-10 18:56:01 +0900

    footer 내용 추가

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -1,6 +1,19 @@
 function Footer() {
   return (
     <footer className="footer">
+      <div className="contact-list">
+        <a
+          href="https://x.com/riize_stream?s=21"
+          target="_blank"
+          rel="noreferrer"
+        >
+          X
+        </a>
+        <a href="https://pf.kakao.com/_LWmxfn" target="_blank" rel="noreferrer">
+          KAKAO
+        </a>
+      </div>
+      <p className="email">riizestreaming@gmail.com</p>
       <p className="copyright">
         &copy; 2025 Riize Streaming. All rights reserved.
       </p>
