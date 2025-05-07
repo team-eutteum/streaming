47b4d6ac5cf05eeba115c56b701dc8282523102f
chobee-joy <chobee0904@gmail.com>
2025-05-07 17:23:54 +0900

    음방 가이드 추가

--- app/lib/constants/voteContent.constants.ts
+++ app/lib/constants/voteContent.constants.ts
@@ -2,6 +2,12 @@ import type { CommonContentProps } from '@/types/guide';
 import voteGuide from 'public/images/vote/collect/collect.jpg';
 import musicDayGuide from 'public/images/vote/music/day.jpg';
 import musicFullGuide from 'public/images/vote/music/full.jpg';
+import musicInkigayoGuide from 'public/images/vote/music/inkigayo.jpg';
+import musicMcountdownGuide from 'public/images/vote/music/mcountdown.jpg';
+import musicMusicBankGuide from 'public/images/vote/music/musicbank.jpg';
+import musicMusicCoreGuide from 'public/images/vote/music/musiccore.jpg';
+import musicShowChampionGuide from 'public/images/vote/music/showchampion.jpg';
+import musicTheShowGuide from 'public/images/vote/music/theshow.jpg';
 
 import * as TITLE from './title.constant';
 
@@ -23,12 +29,32 @@ export const BROADCAST_CONTENT: CommonContentProps = {
   uniqueId: 'voteCollectList',
   content: [
     {
-      label: 'FULL',
-      image: [musicFullGuide],
+      label: '투표일정',
+      image: [musicFullGuide, musicDayGuide],
     },
     {
-      label: 'DAY',
-      image: [musicDayGuide],
+      label: '인기가요',
+      image: [musicInkigayoGuide],
+    },
+    {
+      label: '뮤직뱅크',
+      image: [musicMusicBankGuide],
+    },
+    {
+      label: '음악중심',
+      image: [musicMusicCoreGuide],
+    },
+    {
+      label: '엠카운트다운',
+      image: [musicMcountdownGuide],
+    },
+    {
+      label: '더쇼',
+      image: [musicTheShowGuide],
+    },
+    {
+      label: '쇼챔피언',
+      image: [musicShowChampionGuide],
     },
   ],
 };
