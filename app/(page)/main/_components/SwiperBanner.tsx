a0ebf71b5b23eea40ba46da202f584c276d393bc
chobee-joy <chobee0904@gmail.com>
2025-05-19 15:25:57 +0900

    메인 flyup 추가

--- app/(page)/main/_components/SwiperBanner.tsx
+++ app/(page)/main/_components/SwiperBanner.tsx
@@ -1,30 +1,40 @@
 'use client';
 import Image from 'next/image';
 import Link from 'next/link';
-import { Pagination } from 'swiper/modules';
+import { Pagination, Autoplay } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
 
 function BannerSwiper() {
   return (
     <Swiper
       className="swiper-banner"
-      modules={[Pagination]}
+      modules={[Pagination, Autoplay]}
       speed={600}
+      loop={true}
+      autoplay={{
+        delay: 3000,
+        disableOnInteraction: false,
+      }}
       pagination={{
         clickable: true,
       }}
     >
-      <SwiperSlide className="bg-bbb">
+      <SwiperSlide>
+        <Link
+          href="https://youtu.be/vLUtHODdLzk?si=swRHDlE8IGXsrNCq"
+          target="_blank"
+          className="banner-slide"
+        >
+          <Image src="/images/main/banner/flyup.jpg" alt="Fly Up" fill />
+        </Link>
+      </SwiperSlide>
+      <SwiperSlide>
         <Link
           href="https://youtu.be/vmL-XgxwQZU?si=U38cNrhJT6DsdeLq"
           target="_blank"
           className="banner-slide"
         >
           <Image src="/images/main/banner/bbb.png" alt="Bag Bad Back" fill />
-          <div className="banner-slide-text">
-            <span>Let&apos;s Go</span>
-            <span>&apos;Bag Bad Back&apos; MV</span>
-          </div>
         </Link>
       </SwiperSlide>
     </Swiper>
