8cad5ad828c2e8ebd25788d99843c5712321cd1c
chobee-joy <chobee0904@gmail.com>
2025-06-19 14:08:10 +0900

    투두리스트, 공구 수정

--- app/lib/constants/supportContent.constants.ts
+++ app/lib/constants/supportContent.constants.ts
@@ -1,13 +1,13 @@
 import type { CommonContentProps } from '@/types/guide';
-import album09Guide from 'public/images/support/album/09.jpg';
-import albumAll from 'public/images/support/album/all.jpg';
-import albumALLMDGuide from 'public/images/support/album/ALLMD.jpg';
-import albumAppleGuide from 'public/images/support/album/apple.jpg';
-import albumEverlineGuide from 'public/images/support/album/everline.jpg';
-import albumIttaGuide from 'public/images/support/album/ITTA.jpg';
-import albumKtown4uGuide from 'public/images/support/album/Ktown4u.jpg';
-import albumSoundWaveGuide from 'public/images/support/album/soundwave.jpg';
-import albumYes24Guide from 'public/images/support/album/yes24.jpg';
+// import album09Guide from 'public/images/support/album/09.jpg';
+// import albumAll from 'public/images/support/album/all.jpg';
+// import albumALLMDGuide from 'public/images/support/album/ALLMD.jpg';
+// import albumAppleGuide from 'public/images/support/album/apple.jpg';
+// import albumEverlineGuide from 'public/images/support/album/everline.jpg';
+// import albumIttaGuide from 'public/images/support/album/ITTA.jpg';
+// import albumKtown4uGuide from 'public/images/support/album/Ktown4u.jpg';
+// import albumSoundWaveGuide from 'public/images/support/album/soundwave.jpg';
+// import albumYes24Guide from 'public/images/support/album/yes24.jpg';
 import dontationGuide from 'public/images/support/donation/donation.jpg';
 import helperGuide from 'public/images/support/helper/helper.jpg';
 import idDontationGuide from 'public/images/support/id/id.jpg';
@@ -57,108 +57,111 @@ export const ALBUM_PURCHASE_CONTENT: CommonContentProps = {
   uniqueId: 'albumPurchase',
   content: [
     {
-      label: '모아보기',
-      image: [albumAll],
+      label: TITLE.ALBUM_PURCHASE,
     },
-    {
-      label: 'Ktown4u',
-      image: [albumKtown4uGuide],
-      links: [
-        {
-          link: 'https://kr.ktown4u.com/eventinfo?eve_no=43259949&biz_no=967',
-          linkTxt: 'Ktown4u 공구 바로가기',
-        },
-      ],
-    },
-    {
-      label: '올엠디',
-      image: [albumALLMDGuide],
-      links: [
-        {
-          link: 'https://buly.kr/A44FfrS',
-          linkTxt: '올엠디 공구 바로가기',
-        },
-      ],
-    },
-    {
-      label: '애플뮤직',
-      image: [albumAppleGuide],
-      links: [
-        {
-          link: 'https://abit.ly/smddn4',
-          linkTxt: '애플뮤직 포토북 세트',
-        },
-        {
-          link: 'https://abit.ly/giftvt',
-          linkTxt: '애플뮤직 포토북 랜덤',
-        },
-        {
-          link: 'https://abit.ly/oys2ad',
-          linkTxt: '애플뮤직 패키지 세트',
-        },
-        {
-          link: 'https://abit.ly/76sznu',
-          linkTxt: '애플뮤직 패키지 랜덤',
-        },
-        {
-          link: 'https://abit.ly/d5wopt',
-          linkTxt: '애플뮤직 스미니',
-        },
-      ],
-    },
-    {
-      label: '09PLATFORM',
-      image: [album09Guide],
-      links: [
-        {
-          link: 'https://09platform.com/surl/P/1205',
-          linkTxt: '09플랫폼 포토북 버전',
-        },
-        {
-          link: 'https://09platform.com/surl/P/1206',
-          linkTxt: '09플랫폼 패키지, 스미니',
-        },
-      ],
-    },
-    {
-      label: '예스24',
-      image: [albumYes24Guide],
-      links: [
-        {
-          link: 'https://www.yes24.com/product/category/series/003?SeriesNumber=359260',
-          linkTxt: '예스24 공구 바로가기',
-        },
-      ],
-    },
-    {
-      label: '사운드웨이브',
-      image: [albumSoundWaveGuide],
-      links: [
-        {
-          link: 'https://bit.ly/3Gv6ftz',
-          linkTxt: '사운드웨이브 공구 바로가기',
-        },
-      ],
-    },
-    {
-      label: '에버라인',
-      image: [albumEverlineGuide],
-      links: [
-        {
-          link: 'https://everlineshop.com/goods/goods_view.php?goodsNo=1000012617',
-          linkTxt: '에버라인 공구 바로가기',
-        },
-      ],
-    },
-    {
-      label: 'ITTA(잇다)',
-      image: [albumIttaGuide],
-      links: [
-        {
-          link: 'https://fanitta.com/?value=%7B%22productId%22:25313%7D&type=productDetail',
-          linkTxt: '잇다 공구 바로가기',
-        },
-      ],
-    },
+    // {
+    //   label: '모아보기',
+    //   image: [albumAll],
+    // },
+    // {
+    //   label: 'Ktown4u',
+    //   image: [albumKtown4uGuide],
+    //   links: [
+    //     {
+    //       link: 'https://kr.ktown4u.com/eventinfo?eve_no=43259949&biz_no=967',
+    //       linkTxt: 'Ktown4u 공구 바로가기',
+    //     },
+    //   ],
+    // },
+    // {
+    //   label: '올엠디',
+    //   image: [albumALLMDGuide],
+    //   links: [
+    //     {
+    //       link: 'https://buly.kr/A44FfrS',
+    //       linkTxt: '올엠디 공구 바로가기',
+    //     },
+    //   ],
+    // },
+    // {
+    //   label: '애플뮤직',
+    //   image: [albumAppleGuide],
+    //   links: [
+    //     {
+    //       link: 'https://abit.ly/smddn4',
+    //       linkTxt: '애플뮤직 포토북 세트',
+    //     },
+    //     {
+    //       link: 'https://abit.ly/giftvt',
+    //       linkTxt: '애플뮤직 포토북 랜덤',
+    //     },
+    //     {
+    //       link: 'https://abit.ly/oys2ad',
+    //       linkTxt: '애플뮤직 패키지 세트',
+    //     },
+    //     {
+    //       link: 'https://abit.ly/76sznu',
+    //       linkTxt: '애플뮤직 패키지 랜덤',
+    //     },
+    //     {
+    //       link: 'https://abit.ly/d5wopt',
+    //       linkTxt: '애플뮤직 스미니',
+    //     },
+    //   ],
+    // },
+    // {
+    //   label: '09PLATFORM',
+    //   image: [album09Guide],
+    //   links: [
+    //     {
+    //       link: 'https://09platform.com/surl/P/1205',
+    //       linkTxt: '09플랫폼 포토북 버전',
+    //     },
+    //     {
+    //       link: 'https://09platform.com/surl/P/1206',
+    //       linkTxt: '09플랫폼 패키지, 스미니',
+    //     },
+    //   ],
+    // },
+    // {
+    //   label: '예스24',
+    //   image: [albumYes24Guide],
+    //   links: [
+    //     {
+    //       link: 'https://www.yes24.com/product/category/series/003?SeriesNumber=359260',
+    //       linkTxt: '예스24 공구 바로가기',
+    //     },
+    //   ],
+    // },
+    // {
+    //   label: '사운드웨이브',
+    //   image: [albumSoundWaveGuide],
+    //   links: [
+    //     {
+    //       link: 'https://bit.ly/3Gv6ftz',
+    //       linkTxt: '사운드웨이브 공구 바로가기',
+    //     },
+    //   ],
+    // },
+    // {
+    //   label: '에버라인',
+    //   image: [albumEverlineGuide],
+    //   links: [
+    //     {
+    //       link: 'https://everlineshop.com/goods/goods_view.php?goodsNo=1000012617',
+    //       linkTxt: '에버라인 공구 바로가기',
+    //     },
+    //   ],
+    // },
+    // {
+    //   label: 'ITTA(잇다)',
+    //   image: [albumIttaGuide],
+    //   links: [
+    //     {
+    //       link: 'https://fanitta.com/?value=%7B%22productId%22:25313%7D&type=productDetail',
+    //       linkTxt: '잇다 공구 바로가기',
+    //     },
+    //   ],
+    // },
   ],
 };
--- app/lib/constants/todolistContent.constants.ts
+++ app/lib/constants/todolistContent.constants.ts
@@ -12,33 +12,22 @@ interface TodoListProps {
 }
 
 export const TODOLIST_CONTENT: TodoListProps = {
-  updateDate: '2025.05.19',
-  version: '5', // 수정될 때마다 버전 변경 해줘서 세션에서 변경 사항 감지
+  updateDate: '2025.06.19',
+  version: '6', // 수정될 때마다 버전 변경 해줘서 세션에서 변경 사항 감지
   content: [
     {
       id: 'todoList0',
-      cntn: '멜론,지니,벅스,바이브 6시 개별곡 다운로드',
+      cntn: '하루 1번 Fly Up 감상 🪽',
       checked: false,
     },
     {
       id: 'todoList1',
-      cntn: '멜론,지니,벅스,바이브 8시 FLAC 다운로드',
-      checked: false,
-    },
-    {
-      id: 'todoList2',
       cntn: 'Fly Up MV 스트리밍 하기',
       checked: false,
       url: 'https://youtu.be/vLUtHODdLzk?si=0UFgyDChm3Rx9aCd',
     },
     {
-      id: 'todoList3',
-      cntn: '음악방송 대비 재화 모으기',
-      checked: false,
-      url: '/vote/collect',
-    },
-    {
-      id: 'todoList4',
+      id: 'todoList2',
       cntn: '음총팀 계정 팔로우 / 알림 설정 / 주변 브리즈에게 알리기',
       checked: false,
       url: 'https://x.com/RIIZE_STREAM',
