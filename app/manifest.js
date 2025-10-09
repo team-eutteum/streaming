a960e0e318a7f83b8fe256e05ad80427b735152b
musicS2 <musics2@gmail.com>
2025-10-09 16:44:05 +0900

    로고 크기 수정

--- app/manifest.js
+++ app/manifest.js
@@ -1,19 +1,27 @@
 const manifest = () => {
   return {
-    theme_color: '#222',
-    background_color: '#222',
-    display: 'standalone',
-    scope: '/',
-    start_url: '/redirect',
     name: '음총팀뮤웨',
     short_name: '음총팀뮤웨',
     description: '라이즈 음총팀 뮤웨에 오신 것을 환영합니다',
     lang: 'ko-KR',
+
+    theme_color: '#222222',
+    background_color: '#222222',
+
+    display: 'standalone',
+    orientation: 'portrait',
+    scope: '/',
+    start_url: '/redirect?utm_source=web_app_manifest',
     icons: [
       {
-        src: 'images/logo/logo-app.jpg',
-        sizes: '160x160',
-        type: 'image/jpg',
+        src: 'images/logo/logo-app-192.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        src: 'images/logo/logo-app-512.png',
+        sizes: '512x512',
+        type: 'image/png',
       },
     ],
   };
