5547619f492ca9c018d5ef954f4e673f7f701d99
musicS2 <riizeMusicS2@gmail.com>
2025-12-01 02:12:44 +0900

    공구 닫힘 처리

--- app/lib/constants/supportContent.constants.ts
+++ app/lib/constants/supportContent.constants.ts
@@ -1,7 +1,7 @@
 import type { CommonContentProps } from '@/types/guide';
-import albumAll from 'public/images/support/album/all.jpg';
-import albumALLMDGuide from 'public/images/support/album/ALLMD.jpg';
-import albumKtown4uGuide from 'public/images/support/album/Ktown4u.jpg';
+// import albumAll from 'public/images/support/album/all.jpg';
+// import albumALLMDGuide from 'public/images/support/album/ALLMD.jpg';
+// import albumKtown4uGuide from 'public/images/support/album/Ktown4u.jpg';
 // import dontationGuide from 'public/images/support/donation/donation.jpg';
 // import helperGuide from 'public/images/support/helper/helper.jpg';
 // import idDontationGuide from 'public/images/support/id/id.png';
@@ -70,62 +70,62 @@ export const ALBUM_PURCHASE_CONTENT: CommonContentProps = {
     // {
     //   label: TITLE.ALBUM_PURCHASE,
     // },
-    {
-      label: '모아보기',
-      image: [albumAll],
-    },
-    {
-      label: 'Ktown4u',
-      image: [albumKtown4uGuide],
-      content:
-        '<div class="f-bd3"><ul class="bullet-list"><li class="list">공구처: 케타포</li><li class="list">특전: 미정 (미공포 추가 시 동일 제공 예정)</li><li class="list">가격: 포토북 15,600원 / 챔버 19,200원 / 카탈로그 13,200원 / 스미니 10,800원 (세트 64,200원)</li><li class="list">공구 기간: ~11/30(일) 17:00</li><li class="list">발송일: 11/25 이후</li></ul></div>',
-      links: [
-        {
-          link: 'https://kr.ktown4u.com/eventsub?eve_no=43803989&biz_no=967',
-          linkTxt: 'Ktown4u 공구 바로가기',
-        },
-      ],
-    },
-    {
-      label: '올엠디',
-      image: [albumALLMDGuide],
-      content:
-        '<div class="f-bd3"><ul class="bullet-list"><li class="list">공구처: 올엠디</li><li class="list">가격: 포토북 15,500원 / 챔버 19,000원 / 카탈로그 13,100원 / 스미니 10,700원 (세트 63,600원)</li><li class="list">공구 기간: ~11/30(토) 21:00 (KST)</li><li class="list">발송일: 11/25 이후</li></ul></div>',
-      links: [
-        {
-          link: 'https://bit.ly/47KouW6',
-          linkTxt: '올엠디 공구 바로가기',
-        },
-      ],
-    },
-    {
-      label: '애플뮤직',
-      // image: [albumAppleGuide],
-      content:
-        '<div class="f-bd3"><ul class="bullet-list"><li class="list">공구처: 애플뮤직</li><li class="list">특전: 미정 (미공포 추가 시 동일 제공 예정)</li><li class="list">가격: 포토북 15,400원 / 챔버 18,900원 /카탈로그 13,000원 / 스미니 10,600원 (세트 63,400원)</li><li class="list">공구 기간: ~11/23(일) 23:59</li><li class="list">발송일: 11/24</li></ul></div>',
-      links: [
-        {
-          link: 'https://abit.ly/m6spi3',
-          linkTxt: 'Photobook Ver.',
-        },
-        {
-          link: 'https://abit.ly/bz2bkff',
-          linkTxt: 'Chamber Ver.',
-        },
-        {
-          link: 'https://abit.ly/oyuty1h',
-          linkTxt: 'Catalogue Ver.스마트앨범',
-        },
-        {
-          link: 'https://abit.ly/a3qw8b',
-          linkTxt: 'SMini Ver.스마트앨범/세트',
-        },
-        {
-          link: 'https://abit.ly/s10iwoo',
-          linkTxt: 'SMini Ver.스마트앨범/랜덤',
-        },
-      ],
-    },
+    // {
+    //   label: '모아보기',
+    //   image: [albumAll],
+    // },
+    // {
+    //   label: 'Ktown4u',
+    //   image: [albumKtown4uGuide],
+    //   content:
+    //     '<div class="f-bd3"><ul class="bullet-list"><li class="list">공구처: 케타포</li><li class="list">특전: 미정 (미공포 추가 시 동일 제공 예정)</li><li class="list">가격: 포토북 15,600원 / 챔버 19,200원 / 카탈로그 13,200원 / 스미니 10,800원 (세트 64,200원)</li><li class="list">공구 기간: ~11/30(일) 17:00</li><li class="list">발송일: 11/25 이후</li></ul></div>',
+    //   links: [
+    //     {
+    //       link: 'https://kr.ktown4u.com/eventsub?eve_no=43803989&biz_no=967',
+    //       linkTxt: 'Ktown4u 공구 바로가기',
+    //     },
+    //   ],
+    // },
+    // {
+    //   label: '올엠디',
+    //   image: [albumALLMDGuide],
+    //   content:
+    //     '<div class="f-bd3"><ul class="bullet-list"><li class="list">공구처: 올엠디</li><li class="list">가격: 포토북 15,500원 / 챔버 19,000원 / 카탈로그 13,100원 / 스미니 10,700원 (세트 63,600원)</li><li class="list">공구 기간: ~11/30(토) 21:00 (KST)</li><li class="list">발송일: 11/25 이후</li></ul></div>',
+    //   links: [
+    //     {
+    //       link: 'https://bit.ly/47KouW6',
+    //       linkTxt: '올엠디 공구 바로가기',
+    //     },
+    //   ],
+    // },
+    // {
+    //   label: '애플뮤직',
+    //   // image: [albumAppleGuide],
+    //   content:
+    //     '<div class="f-bd3"><ul class="bullet-list"><li class="list">공구처: 애플뮤직</li><li class="list">특전: 미정 (미공포 추가 시 동일 제공 예정)</li><li class="list">가격: 포토북 15,400원 / 챔버 18,900원 /카탈로그 13,000원 / 스미니 10,600원 (세트 63,400원)</li><li class="list">공구 기간: ~11/23(일) 23:59</li><li class="list">발송일: 11/24</li></ul></div>',
+    //   links: [
+    //     {
+    //       link: 'https://abit.ly/m6spi3',
+    //       linkTxt: 'Photobook Ver.',
+    //     },
+    //     {
+    //       link: 'https://abit.ly/bz2bkff',
+    //       linkTxt: 'Chamber Ver.',
+    //     },
+    //     {
+    //       link: 'https://abit.ly/oyuty1h',
+    //       linkTxt: 'Catalogue Ver.스마트앨범',
+    //     },
+    //     {
+    //       link: 'https://abit.ly/a3qw8b',
+    //       linkTxt: 'SMini Ver.스마트앨범/세트',
+    //     },
+    //     {
+    //       link: 'https://abit.ly/s10iwoo',
+    //       linkTxt: 'SMini Ver.스마트앨범/랜덤',
+    //     },
+    //   ],
+    // },
     // {
     //   label: '09PLATFORM',
     //   image: [album09Guide],
