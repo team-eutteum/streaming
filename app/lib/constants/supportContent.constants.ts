39290b4608d33a997b8f4eb814bfdcfd61e54144
musicS2 <riizeMusicS2@gmail.com>
2026-05-23 10:08:50 +0900

    공구 추가

--- app/lib/constants/supportContent.constants.ts
+++ app/lib/constants/supportContent.constants.ts
@@ -1,12 +1,12 @@
 import type { CommonContentProps } from '@/types/guide';
+import albumALLMDGuide from 'public/images/support/album/ALLMD.jpg';
+import albumKtown4uGuide from 'public/images/support/album/Ktown4u.jpg';
 import dontationGuide from 'public/images/support/donation/donation.jpg';
 import helperGuide from 'public/images/support/helper/helper.jpg';
 import idDontationGuide from 'public/images/support/id/id.jpg';
 import instantIdDontationGuide from 'public/images/support/id/instantId.jpg';
-// import albumKtown4uGuide from 'public/images/support/album/Ktown4u.jpg';
 
 // import albumAll from 'public/images/support/album/all.jpg';
-// import albumALLMDGuide from 'public/images/support/album/ALLMD.jpg";
 
 import * as TITLE from './title.constant';
 export const DONATION_CONTENT: CommonContentProps = {
@@ -76,7 +76,7 @@ export const HELPER_CONTENT: CommonContentProps = {
 
 export const ALBUM_PURCHASE_CONTENT: CommonContentProps = {
   label: TITLE.ALBUM_PURCHASE,
-  updateDate: '2025.11.03',
+  updateDate: '2026.05.22',
   uniqueId: 'albumPurchase',
   content: [
     // {
@@ -86,58 +86,62 @@ export const ALBUM_PURCHASE_CONTENT: CommonContentProps = {
     //   label: '모아보기',
     //   // image: [albumAll],
     // },
-    // {
-    //   label: "Ktown4u",
-    //   // image: [albumKtown4uGuide],
-    //   content:
-    //     '<div class="f-bd3"><ul class="bullet-list"><li class="list">공구처: 케타포</li><li class="list">한정반 A (DVD) / 한정반 B (포토북)<br /> 31,100원 → 공구가 26,400원</li><li class="list">멤버별 한정반, 일반반 <br /> 20,140원 → 공구가 17,100원</li><li class="list">공구 기간: ~소진 시까지</li><li class="list">일본 출시일: 26.02.18</li><li class="list">한국배송: 26.03.20. 이후 순차 발송</li></ul></div>',
-    //   links: [
-    //     {
-    //       link: "https://kr.ktown4u.com/eventinfo?eve_no=43951353&biz_no=967",
-    //       linkTxt: "Ktown4u 공구 바로가기",
-    //     },
-    //   ],
-    // },
-    // {
-    //   label: '올엠디',
-    //   image: [albumALLMDGuide],
-    //   content:
-    //     '<div class="f-bd3"><ul class="bullet-list"><li class="list">공구처: 올엠디</li><li class="list">가격: 포토북 15,500원 / 챔버 19,000원 / 카탈로그 13,100원 / 스미니 10,700원 (세트 63,600원)</li><li class="list">공구 기간: ~11/30(토) 21:00 (KST)</li><li class="list">발송일: 11/25 이후</li></ul></div>',
-    //   links: [
-    //     {
-    //       link: 'https://bit.ly/47KouW6',
-    //       linkTxt: '올엠디 공구 바로가기',
-    //     },
-    //   ],
-    // },
-    // {
-    //   label: '애플뮤직',
-    //   // image: [albumAppleGuide],
-    //   content:
-    //     '<div class="f-bd3"><ul class="bullet-list"><li class="list">공구처: 애플뮤직</li><li class="list">특전: 미정 (미공포 추가 시 동일 제공 예정)</li><li class="list">가격: 포토북 15,400원 / 챔버 18,900원 /카탈로그 13,000원 / 스미니 10,600원 (세트 63,400원)</li><li class="list">공구 기간: ~11/23(일) 23:59</li><li class="list">발송일: 11/24</li></ul></div>',
-    //   links: [
-    //     {
-    //       link: 'https://abit.ly/m6spi3',
-    //       linkTxt: 'Photobook Ver.',
-    //     },
-    //     {
-    //       link: 'https://abit.ly/bz2bkff',
-    //       linkTxt: 'Chamber Ver.',
-    //     },
-    //     {
-    //       link: 'https://abit.ly/oyuty1h',
-    //       linkTxt: 'Catalogue Ver.스마트앨범',
-    //     },
-    //     {
-    //       link: 'https://abit.ly/a3qw8b',
-    //       linkTxt: 'SMini Ver.스마트앨범/세트',
-    //     },
-    //     {
-    //       link: 'https://abit.ly/s10iwoo',
-    //       linkTxt: 'SMini Ver.스마트앨범/랜덤',
-    //     },
-    //   ],
-    // },
+    {
+      label: 'Ktown4u',
+      image: [albumKtown4uGuide],
+      content:
+        '<div class="f-bd3"><ul class="bullet-list"><li class="list">공구처: 케타포</li><li class="list">특전: 미정</li><li class="list">가격: 스페셜 17,200원 / 포토북 랜덤 13,400원 / 포토북 2CD 세트 26,800원 / 스미니 랜덤 11,800원 / 스미니 6CD 세트 69,900원 / 피규어 랜덤 20,800원</li><li class="list">출시일: 2026-06-15</li><li class="list">[세트 상품 판매 기간: 6/14 23:59까지]</li><li class="list">발송일: 6/16일 이후 순차적 발송</li></ul></div>',
+      links: [
+        {
+          link: 'https://kr.ktown4u.com/eventinfo?eve_no=44392603&biz_no=967',
+          linkTxt: 'Ktown4u 공구 바로가기',
+        },
+      ],
+    },
+    {
+      label: '올엠디',
+      image: [albumALLMDGuide],
+      content:
+        '<div class="f-bd3"><ul class="bullet-list"><li class="list">공구처: 올엠디</li><li class="list">특전: 미정</li><li class="list">가격: 스페셜 16,900원 / 포토북 랜덤 13,200원 / 포토북 2종 세트 26,300원 / 스미니 랜덤 11,600원 / 스미니 6종 세트 68,600원 / 피규어 랜덤 20,400원</li><li class="list">출시일: 2026-06-15</li><li class="list">공구 기간: ~06:21 21:00까지</li><li class="list">발송일: 별도 안내</li></ul></div>',
+      links: [
+        {
+          link: 'https://m.allmd.com/product/list.html?cate_no=2520',
+          linkTxt: '올엠디 공구 바로가기',
+        },
+      ],
+    },
+    {
+      label: '애플뮤직',
+      // image: [albumAppleGuide],
+      content:
+        '<div class="f-bd3"><ul class="bullet-list"><li class="list">공구처: 애플뮤직</li><li class="list">특전: 미정 (미공포 추가시 동일 적용 예정)</li><li class="list">가격: 스페셜 17,200원 / 포토북 랜덤 13,300원 / 포토북 2종 세트 26,400원 / 스미니 랜덤 11,800원 / 스미니 6종 세트 69,600원 / 피규어 랜덤 20,600원</li><li class="list">출시일: 2026-06-15</li><li class="list">공구 기간: ~06/14 23:59까지</li><li class="list">발송일: 6/15일 이후 순차 발송</li><li class="list">배송비: 2만원 이상 무료배송(이하 2,500원)</li></ul></div>',
+      links: [
+        {
+          link: 'https://abit.ly/n478ir',
+          linkTxt: 'Photobook Ver.(랜덤)',
+        },
+        {
+          link: 'https://abit.ly/2n0ql6',
+          linkTxt: 'Photobook Ver.(세트)',
+        },
+        {
+          link: 'https://abit.ly/jqc6awp',
+          linkTxt: 'Special Ver.',
+        },
+        {
+          link: 'https://abit.ly/xvfk90',
+          linkTxt: 'SMini Ver.(랜덤)',
+        },
+        {
+          link: 'https://abit.ly/8qeu8c',
+          linkTxt: 'SMini Ver.(세트)',
+        },
+        {
+          link: 'https://abit.ly/hifmvz',
+          linkTxt: 'Figure Ver.(랜덤)',
+        },
+      ],
+    },
     // {
     //   label: '09PLATFORM',
     //   image: [album09Guide],
@@ -152,18 +156,18 @@ export const ALBUM_PURCHASE_CONTENT: CommonContentProps = {
     //     },
     //   ],
     // },
-    // {
-    //   label: '예스24',
-    //   // image: [albumYes24Guide],
-    //   content:
-    //     '<div class="f-bd3"><ul class="bullet-list"><li class="list">공구처: 예스24</li><li class="list">가격: 포토북 17,500원 / 챔버 21,500원 / 카탈로그 14,600원 / 스미니 12,000원 (세트 71,400원)</li><li class="list">공구 기간: ~11/24(일) 17:00</li><li class="list">발송일: 11/25부터 순차배송</li></ul></div>',
-    //   links: [
-    //     {
-    //       link: 'https://yes24.com/product/category/series/003001018002001?SeriesNumber=368390',
-    //       linkTxt: '예스24 공구 바로가기',
-    //     },
-    //   ],
-    // },
+    {
+      label: '예스24',
+      // image: [albumYes24Guide],
+      content:
+        '<div class="f-bd3"><ul class="bullet-list"><li class="list">공구처: 예스24</li><li class="list">특전: 미정 (추가시 동일 제공 예정)</li><li class="list">가격: 스페셜 18,600원 / 포토북 랜덤 14,700원 / 포토북 2종 세트 29,400원 / 스미니 랜덤 12,600원 / 스미니 6종 세트 75,000원 / 피규어 랜덤 22,600원</li><li class="list">출시일: 2026-06-15</li><li class="list">공구 기간: ~06:14 23:59까지</li><li class="list">발송일: 6/15 이후 순차 발송</li></ul></div>',
+      links: [
+        {
+          link: 'https://www.yes24.com/product/category/series/003?SeriesNumber=380057',
+          linkTxt: '예스24 공구 바로가기',
+        },
+      ],
+    },
     // {
     //   label: '사운드웨이브',
     //   image: [albumSoundWaveGuide],
